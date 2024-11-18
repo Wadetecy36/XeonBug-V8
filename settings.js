5bf470e873eab766a0f11f162215c80249e7ce6f
Wadetecy36 <mensahdenzel285@gmail.com>
2024-11-18 21:16:25 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "+233599607733","+233570578965"
+global.ownername = "heiswade"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.location = "Ghana, Accra"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '+233599607733'  //creator number
+global.ownername = 'heiswade' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: +233599607733'
 
 //console view/theme
 global.themeemoji = '🪀'
